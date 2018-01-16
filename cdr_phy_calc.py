6a2001c3ffe8680359d96014b18eab92a03de381
jeepx5 <jimway2002@hotmail.com>
2018-01-16 09:50:39 +0800

    Update cdr_phy_calc.py
    change the dj and eo estimation.
    remove the bert calibration

--- cdr_phy_calc.py
+++ cdr_phy_calc.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as mplt
 from linModel import linModel
-from JTOL_EXT import csv2msk, filABpoint
-import numpy as np
+from JTOL_EXT import csv2msk, filABpoint, csv2mskUP
+
 
 def bertCal(fin):
     mbert = csv2msk(fin)
@@ -20,70 +20,71 @@ def bertCal(fin):
 
 
 cdrph = linModel()
-lst=[1,2,3,5,6,7,9,10,11]
+lst=[3,5,6,7]
 exlst=['A','B','C','D','E','F']
 
+rjlst=[1,2,3]
 pltn=1
 #===calibration============
-fin="D:\\proj\\CPHY_12FF\\LVDS_TEST\\1231\\bert2nd10M.csv"
-callist=bertCal(fin)
+#fin="D:\\proj\\CPHY_12FF\\LVDS_TEST\\1231\\bert2nd10M.csv"
+#callist=bertCal(fin)
 #====cal list gen==========
-
 for cdrset in lst:
-    pgain=np.mod(cdrset, 4)
 
-    fgain=np.floor((cdrset-pgain)/4)
-    print(pgain, fgain)
-    hodb, hoph, freq, jtol = cdrph.cdrStb(0.77, 5.2e9, 36, 2**(pgain+2), 2**(fgain), 2 ** 6, 2 ** 6, 32, 4, 0.183)#20/1000*5.2)
+    for rjsrc in rjlst:
+        pgain=np.mod(cdrset, 4)
 
-    #jtolmask = [[500e3, 1e6, 2e6, 4.9e6, 50e6], [0, 0, 0, 0, 0]]
-    #jtolmask = [[500e3, 1e6, 2e6, 4.9e6, 50e6], [2, 1, 0.5, 0.2, 0.2]]
-    #pth="D:\proj\CPHY_12FF\LVDS_TEST\LVDS_BERT\LVDS_5P2G\VGAEQ00_CDR"
-    #fname = "_ber-10_500M_5mUIRJ5mUIBUJ.csv"
-    #pth='D:\\proj\\CPHY_12FF\\LVDS_TEST\\lvds\\CDR_'
-    #fname='704.csv'
-    pth='D:\\proj\\CPHY_12FF\\LVDS_TEST\\1231\\lvds'
-    fname='704_2nd10M.csv'
+        fgain=np.floor((cdrset-pgain)/4)
+        #pgain=3
+        #fgain=1
+        print(pgain, fgain)
+        rjtt=np.sqrt(0.083*0.083+rjsrc*rjsrc/100/100)
+        print(rjtt)
+        eo=0.75-12*(rjsrc/100)
+        print(eo)
+        hodb, hoph, freq, jtol = cdrph.cdrStb(eo, 5.2e9, 26, 2**(pgain+2), 2**(fgain), 2 ** 6, 2 ** 6, 32, 4,
+                                              rjtt)#20/1000*5.2)
+
+        #jtolmask = [[500e3, 1e6, 2e6, 4.9e6, 50e6], [0, 0, 0, 0, 0]]
+        #jtolmask = [[500e3, 1e6, 2e6, 4.9e6, 50e6], [2, 1, 0.5, 0.2, 0.2]]
+        #pth="D:\proj\CPHY_12FF\LVDS_TEST\LVDS_BERT\LVDS_5P2G\VGAEQ00_CDR"
+        #fname = "_ber-10_500M_5mUIRJ5mUIBUJ.csv"
+        #pth='D:\\proj\\CPHY_12FF\\LVDS_TEST\\lvds\\CDR_'
+        #fname='704.csv'
+        pth='D:\\proj\\CPHY_12FF\\LVDS_TEST\\20180112_phyd+phya\\20180112_phyd+phya\\cdr'
+        cdr_reg='704_RJ'
+        fname='0mUI.csv'
 
 
-    pth4M='D:\\proj\\CPHY_12FF\\LVDS_TEST\\LVDS_5P2G\\VGAEQ00_CDR'
-    fname4M="_ber-10_500M_5mUIRJ5mUIBUJ.csv"
-    if cdrset < 10:
-        cfile = pth+str(cdrset)+fname
-        cf4M=pth4M+str(cdrset)+fname4M
-    else:
-        cdrpos=cdrset - 10
-        cfile=pth+str(exlst[cdrpos])+fname
-        cf4M = pth4M + str(exlst[cdrpos]) + fname4M
-    m={}
-    m4m={}
-    m=csv2msk(cfile)
-    lst=[]
-    #m=filABpoint(m, [10414058,22588636])
 
-    mcal=np.array(m['amp'])+np.array(callist['amp'])
+        if cdrset < 10:
+            cfile = pth+str(cdrset)+cdr_reg+str(rjsrc)+fname
 
-    jtolreal=[m['freq'], mcal]
+        else:
+            cdrpos=cdrset - 10
+            cfile=pth+str(exlst[cdrpos])+cdr_reg+str(rjsrc)+fname
 
-    jtol4M=[m['freq'],m['amp']]
-    #cdrph.jtol(jtolmask, jtol, jtolreal, freq)
-    fig1 = mplt.figure(1)
-    mplt.subplot(3,3,pltn)
-    lms, = mplt.loglog(jtolreal[0], jtolreal[1], 'g^--', lw=2, label='after_cal')
-    lmd, =mplt.loglog(freq, jtol, color='blue', lw=2, label='model')
-    lmk, = mplt.loglog(jtol4M[0], jtol4M[1], 'r+--', lw=2, label='before_cal')
-    #lmk,=mplt.loglog(jtolmask[0], jtolmask[1], color='red', lw=4, label='mask')
-    mplt.legend(handles=[lmd,lmk,lms])
-    #mplt.legend(handles=[lmd, lms])
+        m={}
+        m=csv2mskUP(cfile)
+        lst=[]
+        #m=filABpoint(m, [10414058,22588636])
 
-    mplt.title('2e2 setting is '+str(cdrset), fontsize=10)
-    mplt.grid(True)
-    pltn+=1
+        jtolreal=[m['freq'], m['amp']]
+
+        #cdrph.jtol(jtolmask, jtol, jtolreal, freq)
+        fig1 = mplt.figure(1)
+        mplt.subplot(4,3,pltn)
+        lms, = mplt.loglog(jtolreal[0], jtolreal[1], 'g^--', lw=2, label='after_cal')
+        lmd, =mplt.loglog(freq, jtol, color='blue', lw=2, label='model')
+        #lmk,=mplt.loglog(jtolmask[0], jtolmask[1], color='red', lw=4, label='mask')
+        mplt.legend(handles=[lmd,lms])
+        #mplt.legend(handles=[lmd, lms])
+
+        mplt.title('reg: '+str(cdrset)+'  RJ='+str(rjsrc)+'0mUI', fontsize=10)
+        mplt.grid(True)
+        pltn+=1
 
 mplt.show()
-
-
-
 
 
 # pi analog delay1.2ns(6UI), des=5UI, PI dig=4UI, slicer=1UI
