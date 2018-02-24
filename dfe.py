aafbd54a13d0c39d339c5931251ae6456eda1764
jeepx5 <jimway2002@hotmail.com>
2018-02-24 11:03:43 +0800

    Update dfe.py
    adjust the dac range and resolution

--- dfe.py
+++ dfe.py
@@ -31,16 +31,16 @@ class dfe:
         self.ekin = ekin
         delta = 16.
         dfe_tap = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        dfe_tap[0] = 1. / 128. * 0.48*(self.htaps[0][-1]*128/0.48 + 1. / 8 * delta * self.ekin * (
-        np.sign(self.din[-2]) + 2) / 8)#-1 * np.sign(self.ekin) * np.sign(self.din[-2]) + 2) / 8
+        dfe_tap[0] = 1. / 64. * 0.64*(self.htaps[0][-1]*64/0.64 + 1. / 8 * delta * self.ekin * (
+        np.sign(self.din[-2] + 2) ))#-1 * np.sign(self.ekin) * np.sign(self.din[-2]) + 2) / 8
 
-        dfe_tap[1] = self.htaps[1][-1] + 1. / 16 * delta * np.sign(self.ekin) * np.sign(self.din[-2]) / 128. * 0.48
-        dfe_tap[2] = self.htaps[2][-1] + 1. / 32 * delta * np.sign(self.ekin) * np.sign(self.din[-3]) / 128. * 0.48
-        dfe_tap[3] = self.htaps[3][-1] + 1. / 64 * delta * np.sign(ekin) * np.sign(self.din[-4]) / 128. * 0.48
-        dfe_tap[4] = self.htaps[4][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-5]) / 128. * 0.48
-        dfe_tap[5] = self.htaps[5][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-6]) / 128. * 0.48
-        dfe_tap[6] = self.htaps[6][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-7]) / 128. * 0.48
-        dfe_tap[7] = self.htaps[7][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-8]) / 128 * 8
+        dfe_tap[1] = self.htaps[1][-1] + 1. / 16 * delta * np.sign(self.ekin) * np.sign(self.din[-2]) / 64. * 0.64
+        dfe_tap[2] = self.htaps[2][-1] + 1. / 32 * delta * np.sign(self.ekin) * np.sign(self.din[-3]) / 64. * 0.32
+        dfe_tap[3] = self.htaps[3][-1] + 1. / 64 * delta * np.sign(ekin) * np.sign(self.din[-4]) / 64. * 0.32
+        dfe_tap[4] = self.htaps[4][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-5]) / 64. * 0.16
+        dfe_tap[5] = self.htaps[5][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-6]) / 64. * 0.16
+        dfe_tap[6] = self.htaps[6][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-7]) / 64. * 0.16
+        dfe_tap[7] = self.htaps[7][-1] + 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-8]) / 64 * 0.16
         # dfe_tap[8]=self.htaps[8][-1]+1./128*delta*np.sign(ekin)*np.sign(self.din[-9])/128.*0.48
         return dfe_tap
 
@@ -52,23 +52,22 @@ class dfe:
         delta = 16.
         v_tap = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         #vntap = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        # v_tap[0] = self.vtaps[0][-1] + 1. / 8 * delta * self.ekin / 128. * 0.48 * (
+        # v_tap[0] = self.vtaps[0][-1] + 1. / 8 * delta * self.ekin / 128. * 0.64 * (
         #     -1 * np.sign(self.ekin) * np.sign(self.din[-2]) + 2) / 8
-        v_tap[0] = 1./ 128. * 0.48*(self.vtaps[0][-1]*128/0.48 + 1. / 8 * delta * self.ekin  * (
-            np.sign(self.din[-2]) + 1) / 8)
-        print(str(self.din[-2] + 1))
+        v_tap[0] = 1./ 64. * 0.64*(self.vtaps[0][-1]*64/0.64 + 1. / 8 * delta * self.ekin  * (
+            np.sign(self.din[-2] + 1) ))
         #print(self.vtaps[0][-1],v_tap[0])
 
 
-        v_tap[1] = 0.5*self.vtaps[1][-1] - 1. / 16 * delta * np.sign(self.ekin) * np.sign(self.din[-2]) / 128. * 0.48
+        v_tap[1] = 0.5*self.vtaps[1][-1] - 1. / 16 * delta * np.sign(self.ekin) * np.sign(self.din[-2]) / 64. * 0.64
 
-        v_tap[2] = 0.5*self.vtaps[2][-1] - 1. / 32 * delta * np.sign(self.ekin) * np.sign(self.din[-3]) / 128. * 0.48
-        v_tap[3] = 0.5*self.vtaps[3][-1] - 1. / 64 * delta * np.sign(ekin) * np.sign(self.din[-4]) / 128. * 0.48
-        v_tap[4] = 0.5*self.vtaps[4][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-5]) / 128. * 0.48
-        v_tap[5] = 0.5*self.vtaps[5][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-6]) / 128. * 0.48
-        v_tap[6] = 0.5*self.vtaps[6][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-7]) / 128. * 0.48
-        v_tap[7] = 0.5*self.vtaps[7][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-8]) / 128 * 8
-        # v_tap[8]=self.htaps[8][-1]+1./128*delta*np.sign(ekin)*np.sign(self.din[-9])/128.*0.48
+        v_tap[2] = 0.5*self.vtaps[2][-1] - 1. / 32 * delta * np.sign(self.ekin) * np.sign(self.din[-3]) / 64. * 0.32
+        v_tap[3] = 0.5*self.vtaps[3][-1] - 1. / 64 * delta * np.sign(ekin) * np.sign(self.din[-4]) / 64. * 0.16
+        v_tap[4] = 0.5*self.vtaps[4][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-5]) / 64. * 0.16
+        v_tap[5] = 0.5*self.vtaps[5][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-6]) / 64. * 0.16
+        v_tap[6] = 0.5*self.vtaps[6][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-7]) / 64. * 0.16
+        v_tap[7] = 0.5*self.vtaps[7][-1] - 1. / 128 * delta * np.sign(ekin) * np.sign(self.din[-8]) / 64 * 0.16
+        # v_tap[8]=self.htaps[8][-1]+1./128*delta*np.sign(ekin)*np.sign(self.din[-9])/64.*0.64
 
         #print(v_tap)
         return v_tap
